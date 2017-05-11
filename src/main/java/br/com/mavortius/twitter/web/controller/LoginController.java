5cfc11e6c8ecc432d101c971106d789c610ccdbb
Marcelo Martins <m.a.martins@live.com>
2017-05-11 19:04:18 -0300

    Implemented security.

---
+++ src/main/java/br/com/mavortius/twitter/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package br.com.mavortius.twitter.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String authenticate() {
+        return "login";
+    }
+}
