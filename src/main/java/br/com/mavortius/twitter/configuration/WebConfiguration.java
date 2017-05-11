8551c1bcdcc6d4539c6a7fc0d2fcc3926a5caaf9
Marcelo Martins <m.a.martins@live.com>
2017-05-11 11:18:40 -0300

    Added 'LightTweet' and new date serializers.

--- src/main/java/br/com/mavortius/twitter/configuration/WebConfiguration.java
+++ src/main/java/br/com/mavortius/twitter/configuration/WebConfiguration.java
@@ -1,12 +1,16 @@
 package br.com.mavortius.twitter.configuration;
 
 import br.com.mavortius.twitter.web.formatter.USLocalDateFormatter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -17,6 +21,16 @@ import java.time.LocalDate;
 @Configuration
 @EnableConfigurationProperties({PicturesUploadProperties.class})
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper mapper = builder.createXmlMapper(false).build();
+
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        return mapper;
+    }
 
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
