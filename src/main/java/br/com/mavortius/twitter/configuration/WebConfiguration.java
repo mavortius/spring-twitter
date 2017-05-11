8935c9de2cb324080d0bedca79ac8311ed8a1551
Marcelo Martins <m.a.martins@live.com>
2017-05-11 15:25:46 -0300

    Added Swagger Docs support to Rest API.

--- src/main/java/br/com/mavortius/twitter/configuration/WebConfiguration.java
+++ src/main/java/br/com/mavortius/twitter/configuration/WebConfiguration.java
@@ -15,12 +15,24 @@ import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.util.UrlPathHelper;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 
 @Configuration
+@EnableSwagger2
 @EnableConfigurationProperties({PicturesUploadProperties.class})
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(path -> path.startsWith("/api/"))
+                .build();
+    }
 
     @Bean
     @Primary
